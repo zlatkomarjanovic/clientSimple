8e8894bb992e9aa1746c0bf7b162aac0f877a4e2
BegaDeveloper <ismarbegicit@gmail.com>
2021-06-13 18:06:29 +0200

    heyy

---
+++ Sample/index.js
@@ -0,0 +1,42 @@
+var sku = document.querySelector('#sku');
+var nameOf = document.querySelector('#name');
+var priceOf = document.querySelector('#price');
+
+var size = document.querySelector('#size');
+var height = document.querySelector('#height');
+var width = document.querySelector('#width');
+var lenght = document.querySelector('#lenght');
+var weight = document.querySelector('#weight');
+var listOfDiv = document.querySelector('#listOfDiv');
+var h1Tag = document.querySelector('#h1Tag');
+
+var sku = document.querySelector('#sku');
+
+const addBTN = document.querySelector('#add-product-btn');
+
+addBTN.addEventListener("click", myFunction);
+
+function myFunction(){
+    
+    h1Tag.remove();
+    var divOnDiv = document.createElement("div");
+    divOnDiv.classList.add("divOnDiv");
+    listOfDiv.appendChild(divOnDiv);
+    
+    var scDiv = document.createElement("div");
+    scDiv.classList.add("sc-div");
+    divOnDiv.appendChild(scDiv);
+    
+    scDiv.textContent = sku.value;
+}
+
+
+
+
+
+
+
+
+
+
+
